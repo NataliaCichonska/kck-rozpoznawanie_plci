b2e58f173ff4d7403a7a5f719d8bb3d290bdac5f
MariaBanaszkiewicz <xmox@interia.pl>
2022-12-07 18:02:31 +0100

    zmiany zmiany zmiany

--- inf148174inf148237.py
+++ inf148174inf148237.py
@@ -7,26 +7,29 @@ from scipy.fftpack import fft, fftfreq
 from scipy.signal import decimate
 
 Mmin=85
-Mmax=176
-Kmin=176
+Mmax=171
+Kmin=171
 Kmax=255
 
-def hps(fft):
+def hps(fft, ind, jnd):
     fft_kopia = fft.copy()
     for i in range(2,5):
         d=decimate(fft,i)
-        #dodawaj widma decymowanych wersji do modułu oryginalnego
+        #domnażanie widma decymowanych wersji do modułu oryginalnego
         for j in range(0,len(d)):
-            fft_kopia[j] +=d[j]
+            fft_kopia[j] *=d[j]
+    #tłumienie szumów i wybór podstawowej częstotliwości
+    fft_kopia[:ind] = 0
+    fft_kopia[jnd:] = 0
     indeks = np.argmax(fft_kopia)
     return indeks
 
-def uzyskaj_indeks_czestotliwosci_podstawowej(sygnal):
+def uzyskaj_indeks_czestotliwosci_podstawowej(sygnal, i, j):
     #kaiser
     sygnal=sygnal*np.kaiser(len(sygnal),beta=14)
     sygnal=abs(fft(sygnal))
     #hps
-    indeks=hps(sygnal)
+    indeks=hps(sygnal, i, j)
     return indeks
 
 #zwraca sygnał i częstotliwość próbkowania
@@ -37,18 +40,26 @@ def czytaj_wav(nazwa_pliku):
         sygnal=(sygnal[:,0] + sygnal[:,1])/2
     return sygnal, czestotliowsc_prob
 
-#printuje "M", jeśli głos należy do mężczyzny, a "K", jeśli należy do kobiety
+#zwraca "M", jeśli głos należy do mężczyzny, a "K", jeśli należy do kobiety
 def rozpoznaj(nazwa_pliku):
     sygnal, czestotliwosc_prob = czytaj_wav(nazwa_pliku)
-    fx=fftfreq(len(sygnal), d=1/czestotliwosc_prob)
-    indeks=uzyskaj_indeks_czestotliwosci_podstawowej(sygnal)
-    czestotliowsc_podstawowa=fx[indeks]
-    if (czestotliowsc_podstawowa>=Mmin and czestotliowsc_podstawowa<=Mmax):
-        print("M")
-        return("M")
-    else:
-        print("K")
-        return("K")
+    fx = np.linspace(0,czestotliwosc_prob,num=len(sygnal),endpoint=False)
+
+    #wyznaczanie indeksów szumu
+    for i in range(len(fx)):
+        if (fx[i]>Mmin):
+            break
+    for j in range(i,len(fx)):
+        if (fx[j]>Kmax):
+            break
+
+    indeks =uzyskaj_indeks_czestotliwosci_podstawowej(sygnal, i,j)
+    czestotliowsc_podstawowa= fx[indeks]
+
+    if (czestotliowsc_podstawowa<=Mmax):
+        return "M"
+    else :
+        return "K"
 
 if __name__ == '__main__':
         nazwa_pliku = sys.argv[1]
@@ -59,9 +70,11 @@ if __name__ == '__main__':
                 wszystkie+=1
                 wynik=rozpoznaj(filename)
                 if(filename.split("_")[1].split(".")[0]!=wynik):
-                    # print("niepoprawne: "+filename)
                     niepoprawne+=1
+                    print(filename, wynik)
             print(niepoprawne,wszystkie)
+            print("accuracy: ", (wszystkie-niepoprawne)/wszystkie)
 
         else:
-            rozpoznaj(nazwa_pliku)
+            wynik = rozpoznaj(nazwa_pliku)
+            print(wynik)
